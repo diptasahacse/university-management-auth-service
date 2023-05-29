2ddc9c5f64ee5e22b175adc7312a35e1af280f38
diptasahacse <diptasaha.lpu.cse@gmail.com>
2023-05-30 01:36:09 +0600

    test

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 
-          const app: Application = express()
+const app: Application = express()
 
 // Cors
 app.use(cors())
