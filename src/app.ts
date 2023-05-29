4099ae921c70501fe779b271700ab469a8f4b7e6
diptasahacse <diptasaha.lpu.cse@gmail.com>
2023-05-30 01:12:48 +0600

    test

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,19 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 
-
-       const app: Application = express()
+          const app: Application = express()
 
 // Cors
 app.use(cors())
 
 // Parser
-      app.use(express.json())
+app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 // Database Connect
 
-
 // Default Route
-app.get('/', (req:Request, res:Response) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
 
