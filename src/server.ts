39377de3fe6f318c4e4dcaac14e4d30ed37971c1
diptasahacse <diptasaha.lpu.cse@gmail.com>
2023-06-10 02:06:26 +0600

    Error handler setup initially done

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,24 @@ import app from './app'
 import config from './config/index'
 import { Server } from 'http'
 import { logger, errorLogger } from './shared/logger'
+let server: Server
+
+// Uncaught Exception Handler
+/*
+Synchronous Error
+This handler handler error which made by developer mistake.
+
+For Example,
+let x = 5;
+console.log(y)
+
+*/
+process.on('uncaughtException', error => {
+  errorLogger.error(error)
+  process.exit(1)
+})
+
 const startServer = async () => {
-  let server: Server
   try {
     await mongoose.connect(config.database_uri as string)
     logger.info('Database is Connected')
@@ -15,9 +31,9 @@ const startServer = async () => {
   } catch (error) {
     errorLogger.error('Error', error)
   }
-
+  // Unhandled Rejection Handler
+  // Async Error
   process.on('unhandledRejection', error => {
-    console.log('Unhandled Rejection detected, We are closing the connection')
     if (server) {
       server.close(() => {
         errorLogger.error(error)
@@ -29,3 +45,14 @@ const startServer = async () => {
   })
 }
 startServer()
+
+// Signal Termination
+/*
+I can terminate the server by sending signal
+*/
+process.on('SIGTERM', () => {
+  logger.info('Sigterm is received')
+  if (server) {
+    server.close()
+  }
+})
