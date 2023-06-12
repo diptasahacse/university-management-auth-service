17f4ece32dae8616adfbd7fe65c17774b0641f46
diptasahacse <diptasaha.lpu.cse@gmail.com>
2023-06-13 00:30:42 +0600

    handle same year and same semester conflict using pre hook

--- src/app/modules/academicSemester/academicSemester.model.ts
+++ src/app/modules/academicSemester/academicSemester.model.ts
@@ -1,4 +1,5 @@
 import { Schema, model } from 'mongoose';
+import status from 'http-status';
 import {
   AcademicSemesterModel,
   IAcademicSemester,
@@ -8,6 +9,7 @@ import {
   academicSemesterMonths,
   academicSemesterTitles,
 } from './academicSemester.constant';
+import ApiError from '../../../errors/ApiError';
 
 const academicSemesterSchema = new Schema<IAcademicSemester>(
   {
@@ -40,6 +42,24 @@ const academicSemesterSchema = new Schema<IAcademicSemester>(
     timestamps: true,
   }
 );
+// Pre Hook => When apply hook before save
+// Post Hook => When apply hook after save
+// Handling Same Year and Same Semester issues using pre hook.
+// Always use before model
+
+academicSemesterSchema.pre('save', async function (next) {
+  const isExist = await AcademicSemester.findOne({
+    title: this.title,
+    year: this.year,
+  });
+  if (isExist) {
+    throw new ApiError(
+      status.CONFLICT,
+      'Academy semester type and year are already exists!'
+    );
+  }
+  next();
+});
 
 export const AcademicSemester = model<IAcademicSemester, AcademicSemesterModel>(
   'AcademicSemester',
