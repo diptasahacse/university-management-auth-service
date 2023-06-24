10aa2ee95b93027a5e4bf3a6e223cc6a33f52492
diptasahacse <diptasaha.lpu.cse@gmail.com>
2023-06-24 12:03:27 +0600

    make all student api working

--- src/app/modules/student/student.route.ts
+++ src/app/modules/student/student.route.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 // import validateRequest from '../../middlewares/validateRequest';
 // import { UserValidation } from '../user/user.validation';
 import { StudentController } from './student.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import { StudentValidation } from './student.validation';
 
 const router = Router();
 // Student
@@ -12,10 +14,10 @@ router.get('/:id', StudentController.getSingleStudent);
 router.get('/', StudentController.getAllStudent);
 router.delete('/:id', StudentController.deleteStudent);
 
-// router.patch(
-//   '/create-student',
-//   validateRequest(UserValidation.createStudentZodSchema),
-//   StudentController.createStudent
-// );
+router.patch(
+  '/:id',
+  validateRequest(StudentValidation.updateStudentZodSchema),
+  StudentController.updateStudent
+);
 
 export const StudentRoutes = router;
